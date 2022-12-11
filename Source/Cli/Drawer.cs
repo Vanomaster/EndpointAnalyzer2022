9f16c98484389f3c7f4e68a52e81a52ad56fe695
maksimTemp <j411kx5pb@relay.firefox.com>
2022-12-11 17:58:08 +0700

    feat: added Cli template

---
+++ Source/Cli/Drawer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cli;
+
+public class Drawer
+{
+    public static int index = 0;
+
+    public int DrawMenu(List<string> items)
+    {
+        Console.CursorVisible = false;
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (i == index)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine(items[i]);
+            }
+            else
+            {
+                Console.WriteLine(items[i]);
+            }
+            Console.ResetColor();
+        }
+
+        ConsoleKeyInfo ckey = Console.ReadKey();
+        // ckey.Key switch
+        // {
+        //     ConsoleKey.DownArrow => _ =>
+        //     {
+        //         if (index == items.Count - 1)
+        //         {
+        //             index = 0;
+        //         }
+        //         else
+        //         {
+        //             index++;
+        //         }
+        //     },
+        //     ConsoleKey.UpArrow => _ =>
+        //     {
+        //         if (index <= 0)
+        //         {
+        //             index = items.Count - 1;
+        //         }
+        //         else
+        //         {
+        //             index--;
+        //         }
+        //     },
+        //     ConsoleKey.Enter => index,
+        //     _ => -1,
+        // };
+        // return -2;
+        switch (ckey.Key)
+        {
+            case ConsoleKey.DownArrow:
+            {
+                if (index == items.Count - 1)
+                {
+                    index = 0;
+                }
+                else { index++; }
+            }
+                break;
+            case ConsoleKey.UpArrow:
+            {
+                if (index <= 0)
+                {
+                    index = items.Count - 1;
+                }
+                else { index--; }
+            }
+                break;
+            case ConsoleKey.Enter:
+            {
+                return index;
+            }
+            default:
+            {
+                return -1;
+            }
+        }
+        return -2;
+    }
+}
--- Source/Cli/InputHandler.cs
+++ Source/Cli/InputHandler.cs
@@ -1,6 +1,3 @@
-﻿using Core;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Cli;
 
 /// <summary>
@@ -8,6 +5,34 @@ namespace Cli;
 /// </summary>
 public class InputHandler
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InputHandler"/> class.
+    /// </summary>
+    /// <param name="hardwareAnalyzer">Hardware analyzer.</param>
+    public InputHandler(HardwareAnalyzer hardwareAnalyzer)
+    {
+        HardwareAnalyzer = hardwareAnalyzer;
+    }
+
+    private HardwareAnalyzer HardwareAnalyzer { get; }
+
+    private static readonly List<string> MenuItems = new()
+    {
+        "Проверка параметров учётных записей",
+        "анализ 2",
+        "анализ 3",
+        "Выйти",
+    };
+
+    private static readonly List<string> MenuItemsSoftware = new()
+    {
+        "Анализ 1",
+        "анализ 2",
+        "Назад",
+    };
+
+    private Drawer Drawer { get; set; }
+
     /// <summary>
     /// Initializes a new instance of the <see cref="InputHandler"/> class.
     /// </summary>
@@ -27,5 +52,105 @@ public class InputHandler
         var dbUpdaterFromScvFiles = ServiceProvider.GetRequiredService<DbUpdaterFromScvFiles>();
         string result = dbUpdaterFromScvFiles.UpdateAll();
         Console.WriteLine(result);
+        while (true)
+        {
+            DrawMainMenu();
+        }
+    }
+
+
+    public void DrawMainMenu()
+    {
+        int selectedMenuItem = Drawer.DrawMenu(MenuItems);
+        switch (selectedMenuItem)
+        {
+            case -2:
+            {
+                Console.Clear();
+            }
+                break;
+            case -1:
+            {
+                Console.Clear();
+                Console.WriteLine("Выберите один вариант с помощью клавиш вверх/вниз и введите для подтверждения");
+                Thread.Sleep(3000);
+                Console.Clear();
+                Drawer.DrawMenu(MenuItems);
+            }
+                break;
+            case 0: //GPPARAM
+            {
+                DispayRecommendations(Analyzer, "");
+            }
+                break;
+            case 1: // HARDWARE
+            {
+                DispayRecommendations(Analyzer, "");
+            }
+                break;
+            case 2: // SOFTWARE
+            {
+                DrawSoftwareMenu();
+            }
+                break;
+            default: // выход
+            {
+                Environment.Exit(0);
+            }
+                break;
+        }
+    }
+
+    private void DrawSoftwareMenu()
+    {
+        while (true)
+        {
+            int selectedMenuItem = Drawer.DrawMenu(MenuItemsSoftware);
+            switch (selectedMenuItem)
+            {
+                case -2:
+                {
+                    Console.Clear();
+                }
+                    break;
+                case -1:
+                {
+                    Console.Clear();
+                    Console.WriteLine("Выберите один вариант с помощью клавиш вверх/вниз и введите для подтверждения");
+                    Thread.Sleep(3000);
+                    Console.Clear();
+                    Drawer.DrawMenu(MenuItemsSoftware);
+                }
+                    break;
+                case 0: // SOFTWAREUPD
+                {
+                    DispayRecommendations(Analyzer, "");
+                }
+                    break;
+                case 1: // SOFTWARE
+                {
+                    DispayRecommendations(Analyzer, "");
+                }
+                    break;
+                default: // выход
+                {
+                    Console.Clear();
+                    Drawer.DrawMenu(MenuItems);
+                }
+                    break;
+            }
+        }
+    }
+
+    private void DispayRecommendations(IAnalyzer<List<string>> analyzer, string analayzerName)
+    {
+        Console.Clear();
+        Console.WriteLine($"{analayzerName} вывел следующие рекоммендации:");
+        Console.Write(analyzer.Analyze());
+        Console.WriteLine("\n_____________________________________");
+        Console.WriteLine("Нажмите любую клавишу для продолжения");
+        Thread.Sleep(3000);
+        Console.ReadKey();
+        Console.Clear();
     }
 }
